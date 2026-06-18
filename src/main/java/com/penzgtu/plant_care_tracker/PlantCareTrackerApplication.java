38e93dcb690c5bea46a4053e6a810ce98befb5a7
AlexanderAM <alxmiron58@yandex.ru>
2026-06-18 12:05:12 +0300

    init

---
+++ src/main/java/com/penzgtu/plant_care_tracker/PlantCareTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.penzgtu.plant_care_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantCareTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/penzgtu/plant_care_tracker/PlantCareTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penzgtu.plant_care_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCareTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
